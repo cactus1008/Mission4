6c8aebf4ce29ca25a7adcdc93d44c5759ed01efb
cactus1008 <joaquin.n.elizalde@gmail.com>
2026-01-27 11:30:37 -0700

    Added instruction comments

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
 ﻿// Method Signatures:
 
+/* 
+ * Welcome the user to the game
+ * Create a game board array to store the players’ choices
+ * Ask each player in turn for their choice and update the game board array
+ * Print the board by calling the method in the supporting class
+ * Check for a winner by calling the method in the supporting class, and notify the players
+ *      when a win has occurred and which player won the game
+ *      
+ * 2) The supporting class (with whichever name you choose) will:
+ * Receive the “board” array from the driver class
+ * Contain a method that prints the board based on the information passed to it
+ * Contain a method that receives the game board array as input and returns if there is a winner and who it was
+ */
