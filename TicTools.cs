b36d66430732b74279b699efd5a682bee69d9bf3
Gsafsten <Garrett.safsten@gmail.com>
2026-01-28 08:02:33 -0700

    Copy and Pasted code

--- TicTools.cs
+++ TicTools.cs
@@ -6,5 +6,15 @@ namespace Mission4
 {
     internal class TicTools
     {
+        // Method signature #1
+        public string[9] PrintBoard(string[] boardArray, int position, bool xo);
+        {
+            // Method implementation
+        }
+        // Method signature #2
+        public string CheckWinner(string[] boardArray);
+        {
+            // Method implementation
+        }
     }
 }
