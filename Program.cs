3c9468b2d0e444ee76fb01f53980fd92a3985632
dawsonfbroadbent <dawsonfbroadbent@gmail.com>
2026-01-28 09:58:10 -0700

    Program Updates

--- Program.cs
+++ Program.cs
@@ -1,59 +1,80 @@
-﻿// Method Signatures:
+﻿//Mission 4 Assignment
+// Dawson Broadbent, Joaquin Elizalde, Michael Jones, Garrett Safsten
 
-/* 
- * Welcome the user to the game
- * Create a game board array to store the players’ choices
- * Ask each player in turn for their choice and update the game board array
- * Print the board by calling the method in the supporting class
- * Check for a winner by calling the method in the supporting class, and notify the players
- *      when a win has occurred and which player won the game
- *      
- * 2) The supporting class (with whichever name you choose) will:
- * Receive the “board” array from the driver class
- * Contain a method that prints the board based on the information passed to it
- * Contain a method that receives the game board array as input and returns if there is a winner and who it was
- */
-
-// Welcome the user to the game
+//Using Statement
 using Mission4;
 using System.Threading.Tasks;
 
+//Variable declarations
 bool gameOver = false;
 int position = 0;
+char player = 'X';
+int turnNumber = 1;
 char[] gameBoard = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 string tempPosition = "";
 
+//Declare and instantiate an instance of our class
 TicTools tt = new TicTools();
 
+// Welcome the user to the game
 Console.WriteLine("Welcome to Tic-Tac-Toe!");
+tt.PrintBoard(gameBoard);
 
 // Game flow
 do
 {
-    tt.PrintBoard(gameBoard);
-    // Ask each player in turn for their choice and update the game board array
-    Console.WriteLine("Player 1 (X's), choose a position:");
-    tempPosition = Console.ReadLine();
-    if(int.TryParse(tempPosition, out position) && (position > 0) && (position < 10))
+    if (turnNumber % 2 == 1)
     {
-        gameBoard[position - 1] = 'X';
+        player = 'X';
     }
     else
     {
-        Console.WriteLine("Not a valid position. Please enter a valid integer");
+        player = 'O';
     }
-
-
-    // After player 2:
-
+    
+    //Print Updated Game Board
     tt.PrintBoard(gameBoard);
-    tt.CheckWinner(gameBoard);
+    
+    // Ask each player in turn for their choice and update the game board array
+    Console.WriteLine($"Player {player}'s turn, choose a position:");
+    tempPosition = Console.ReadLine();
+    
+    //Check for a valid selection
+    bool isValid = int.TryParse(tempPosition, out position) && (position > 0) && (position < 10);
+    if(!isValid)
+    {
+        //Error message if invalid
+        Console.WriteLine("Not a valid position. Please enter a valid integer");
+        
+    }
+    //Game Logic
+    else
+    {
+        //Replace position with X or O based on player
+        gameBoard[position - 1] = player; 
+        
+        //Increment turn
+        turnNumber++;
+        
+        //Check for winners
+        tt.CheckWinner(gameBoard);
+    }
 } while (!gameOver);
 
-// Method signature #1
 
+/*
+* 2) The supporting class (with whichever name you choose) will:
+* Receive the “board” array from the driver class
+* Contain a method that prints the board based on the information passed to it
+    * Contain a method that receives the game board array as input and returns if there is a winner and who it was
+    */
+// Method signature #1
+//public string PrintBoard(char[] boardArray);
+{
+    // Method implementation
+}
 // Method signature #2
-public string CheckWinner(char[] boardArray);
+//public string CheckWinner(char[] boardArray);
 {
     // Method implementation
 }
